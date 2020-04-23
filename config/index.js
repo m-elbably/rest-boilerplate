4501f4088535907ba294cb5db9855205e8e1036a
ahmed_elbassel <elbassel.n13@gmail.com>
2020-04-23 12:02:50 +0200

    Fixing linting indentation

--- config/index.js
+++ config/index.js
@@ -1,10 +1,11 @@
 const development = require('./config.dev');
 const production = require('./config.prod');
+
 const env = process.env.NODE_ENV || 'development';
 
 const configurations = {
-    development,
-    production
+  development,
+  production
 };
 
 module.exports = configurations[env];
